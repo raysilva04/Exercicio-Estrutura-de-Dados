1a63f33363a002c4b84f6d08719db0d17e953a16
Rayane Silva <raysaraujo1999@gmail.com>
2026-03-19 09:17:59 -0300

    Create Questão 1.C

---
+++ Questão 1.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    
+    int i[6] = {1,2,3,4,5,6};
+   
+    for(int i = 0; i < 6; i++) {
+    printf(" A impressão dos 5 primeiros números naturais:  %d \n", i);
+    }
+    
+    return 0;
+}
