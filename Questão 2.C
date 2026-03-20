d6f395efd6d92ca59a108cb77776f7bb0cb21c89
Rayane Silva <raysaraujo1999@gmail.com>
2026-03-19 22:45:46 -0300

    Create Questão 2.C

---
+++ Questão 2.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+   
+   int i;
+   
+   for(i = -1; i >= -5; i--) {
+       printf("\n A impressão dos 5 primeiros números negativos: %d\n", i);
+   }
+   
+    return 0;
+}
